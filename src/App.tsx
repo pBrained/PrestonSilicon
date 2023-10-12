85f9f848983e874add0576787bac2a62bedfdbe1
pBrained <115773222+pBrained@users.noreply.github.com>
2023-10-12 03:48:28 -0700

    Added the backgorund svg that we want to animate and got it going also began working on getting the project together with a bootstrap navagation bar!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import background from './img/BG-01.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background" style={{ backgroundImage: `url(${background})`}}>
+          {
+            <img src={logo} className='App-logo'></img>
+          }
+      </div>
     </div>
   );
 }
