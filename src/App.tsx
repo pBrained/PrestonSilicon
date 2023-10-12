5bba254bf52ce8e5b2596c197f38e941058b44e5
pBrained <115773222+pBrained@users.noreply.github.com>
2023-10-12 14:25:10 -0700

    Starting the day with components and also am going to improve bacground processing!

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
     <div className="App">
       <div className="background" style={{ backgroundImage: `url(${background})`}}>
           {
-            <img src={logo} className='App-logo'></img>
           }
       </div>
     </div>
