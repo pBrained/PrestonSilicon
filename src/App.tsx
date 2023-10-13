e297ffbfb91b32d6929b6a43d2926110dc63182b
pBrained <115773222+pBrained@users.noreply.github.com>
2023-10-13 12:38:10 -0700

    One Line

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import logo from './img/logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import logo from './img/logo.svg';
 import background from './img/BG-01.svg';
+import linkedinL from './img/LI-In-Bug.png'
 
 function App() {
   return (
     <div className="App">
       <div className="background" style={{ backgroundImage: `url(${background})`}}>
           {
-            <img src={logo} className="App-logo" alt="logo"/>
+            <div>
+              <img src={logo} className="App-logo" alt="logo"/>
+              <img src={linkedinL} className="App-logo" alt="logo" />
+            </div>
           }
       </div>
     </div>
